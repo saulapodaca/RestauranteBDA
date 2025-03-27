a55d8defcc1d8b906420b0beb141a25caec6e731
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-03-27 15:42:48 -0700

    Creacion del proyecto

---
+++ SistemaRestaurantePersistencia/src/main/java/itson/sistemarestaurantepersistencia/SistemaRestaurantePersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.sistemarestaurantepersistencia;
+
+/**
+ *
+ * @author saula
+ */
+public class SistemaRestaurantePersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
